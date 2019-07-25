4c9cc54638680e15362bb3e92dc681d2572285cf
UlaganathanMuthulingam <ulaganathanm.22@gmail.com>
2019-07-25 12:50:39 +0530

    Commit new file and update

--- APITesting/src/test/java/com/maveric/restapi/CitrusTesting.java
+++ APITesting/src/test/java/com/maveric/restapi/CitrusTesting.java
@@ -1,26 +1,12 @@
 package com.maveric.restapi;
 
 
-import org.testng.annotations.Test;
 
-import com.consol.citrus.annotations.CitrusTest;
-import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
-
-
-public class CitrusTesting extends TestNGCitrusTestDesigner{
+public class CitrusTesting {
 	
 public static void main(String args[])
 {
-	CitrusTesting citrus=new CitrusTesting();
-	citrus.loggingTest();
-	
+	System.out.println("Hello this is for gitub");	
 }
-
-	  public void loggingTest() {
-		echo("Before loggingService call");
-		  
-		  echo("After loggingService call");
-	  }
-
 
 }
---
+++ APITesting/src/test/java/com/maveric/restapi/NewFile.java
@@ -0,0 +1,10 @@
+package com.maveric.restapi;
+
+public class NewFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
